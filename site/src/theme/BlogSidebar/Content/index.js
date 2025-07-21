7c000eefdfdb75feeb065381d4faee3545489a86
Glass Panel <63582311+glass-panel@users.noreply.github.com>
2025-07-21 10:48:34 +0800

    Remove year display for blog sidebar (#30)
    * Remove year display for blog sidebar

---
+++ site/src/theme/BlogSidebar/Content/index.js
@@ -0,0 +1,33 @@
+import React, {memo} from 'react';
+import {useThemeConfig} from '@docusaurus/theme-common';
+import {groupBlogSidebarItemsByYear} from '@docusaurus/plugin-content-blog/client';
+import Heading from '@theme/Heading';
+function BlogSidebarYearGroup({year, yearGroupHeadingClassName, children}) {
+  return (
+    <div role="group">
+      
+      {children}
+    </div>
+  );
+}
+function BlogSidebarContent({items, yearGroupHeadingClassName, ListComponent}) {
+  const themeConfig = useThemeConfig();
+  if (themeConfig.blog.sidebar.groupByYear) {
+    const itemsByYear = groupBlogSidebarItemsByYear(items);
+    return (
+      <>
+        {itemsByYear.map(([year, yearItems]) => (
+          <BlogSidebarYearGroup
+            key={year}
+            year={year}
+            yearGroupHeadingClassName={yearGroupHeadingClassName}>
+            <ListComponent items={yearItems} />
+          </BlogSidebarYearGroup>
+        ))}
+      </>
+    );
+  } else {
+    return <ListComponent items={items} />;
+  }
+}
+export default memo(BlogSidebarContent);
